10ca48500487b4b6d442fbac6cd66d18b9edf0b0
Mace <maceplus@gmail.com>
2021-06-08 14:02:10 +0100

    add circle of buttons

--- src/components/HexButtons.js
+++ src/components/HexButtons.js
@@ -1,18 +1,41 @@
+import React, { useRef, useEffect } from "react";
 
-export const Buttons = () => {        
+
+export const Buttons = () => {    
+    const graph = useRef(null);
+
+  useEffect(() => {
+    const ciclegraph = graph.current;
+    const circleElements = ciclegraph.childNodes;
+
+    let angle = 360 - 90;
+    let dangle = 360 / circleElements.length;
+
+    for (let i = 0; i < circleElements.length; i++) {
+      let circle = circleElements[i];
+      angle += dangle;
+      circle.style.transform = `rotate(${angle}deg) translate(${ciclegraph.clientWidth /
+        2}px) rotate(-${angle}deg)`;
+    }
+  }, []);
+    
+
   const hex = () => {
       let hexCode = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
       return hexCode.map((value) => {
-          return <button>{value}</button>
+          return <button className="circle">{value}</button>
         })                 
     };
 
     return (
-        <div>
-            {hex()}
+        <div className="round">
+            <div className="ciclegraph" ref={graph}>
+                {hex()}
+            </div>
         </div>
     )
 };
+
 
 
 //   export function Buttons() {        

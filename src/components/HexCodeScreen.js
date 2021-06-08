0ca3691af11fe0783538bf2fbfa10a51b58662c2
Mace <maceplus@gmail.com>
2021-06-09 00:14:14 +0100

    screen for hex

---
+++ src/components/HexCodeScreen.js
@@ -0,0 +1,15 @@
+export const Screen2 = () => {
+  return (
+    <div>
+      <form>
+        <label>
+          Hex Code:
+          <input type="text" />{" "}
+        </label>
+        <input type="submit" value="Convert" />
+      </form>
+    </div>
+  );
+};
+
+//

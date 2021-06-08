29575bc38b04dc1e6b1e420720039f91babd2265
Mace <maceplus@gmail.com>
2021-06-08 17:01:33 +0100

    add Screen1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Header } from './components/Header'
 import { Buttons } from "./components/HexButtons";
+import { Screen1 } from "./components/MarksScreen"
 import { Footer } from "./components/Footer";
 
 
@@ -8,6 +9,7 @@ function Rover() {
   return (
     <div className="App">
       <Header />
+      <Screen1 />
       <Buttons />
       <Footer />
       

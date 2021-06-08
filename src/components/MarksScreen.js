5add293a7708e4f996de34b01615073631e594ef
Mace <maceplus@gmail.com>
2021-06-08 17:00:54 +0100

    add astonaut screen

---
+++ src/components/MarksScreen.js
@@ -0,0 +1,17 @@
+export const Screen1 = () => {
+
+  const name = "Watney"
+
+  return (
+    
+    <div>
+      <form>
+        <label>
+          Astronaut {name}:
+          <input type="text" />{" "}
+        </label>
+        <input type="submit" value="Send Message" />
+      </form>
+    </div>
+  );
+};

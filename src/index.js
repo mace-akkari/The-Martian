7d460473e18072f369c52895b83885ff4083266d
Kass Akkari <kass@kassems-mbp.home>
2021-06-14 20:16:39 +0100

    rename Rover to App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Rover from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Rover />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
